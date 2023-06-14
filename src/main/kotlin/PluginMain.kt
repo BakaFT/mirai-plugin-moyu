c53ade08a5be5d06bce02aebbd86c8e323ecea11
BakaFT <bakaft@outlook.com>
2023-06-15 02:45:13 +0800

    Add EventHandlers

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -7,6 +7,12 @@ import command.SendGroupCommand
 import net.mamoe.mirai.console.command.*
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.contact.AnonymousMember
+import net.mamoe.mirai.contact.Group
+import net.mamoe.mirai.contact.remarkOrNick
+import net.mamoe.mirai.event.GlobalEventChannel
+import net.mamoe.mirai.event.events.*
+import net.mamoe.mirai.message.data.content
 import net.mamoe.mirai.utils.info
 
 object PluginMain : KotlinPlugin(
@@ -32,7 +38,31 @@ object PluginMain : KotlinPlugin(
         CommandManager.registerCommand(SearchCommand)
 
         // EventHandlers
+        val eventChannel = GlobalEventChannel.parentScope(this)
+        // Console主动发送
+        eventChannel.subscribeAlways<GroupMessagePostSendEvent> {
+            println("[群][${target.name}(${target.id})] <- $message")
+        }
+        eventChannel.subscribeAlways<FriendMessagePostSendEvent> {
+            println("[好友][${target.remarkOrNick}(${target.id})] <- $message")
+        }
 
+        // Console 被动接受
+        eventChannel.subscribeAlways<GroupMessageEvent> {
+            val displayId = if (sender is AnonymousMember) "匿名" else sender.id.toString()
+            println("[群][${group.name}(${group.id})] $senderName($displayId) -> ${message.content}")
+        }
+        eventChannel.subscribeAlways<FriendMessageEvent> {
+            println("[好友][${sender.remarkOrNick}(${sender.id})] -> ${message.content}")
+        }
+
+        // 同步机器人在其他客户端的发送
+        eventChannel.subscribeAlways<GroupMessageSyncEvent> {
+            println("[群][${group.name}(${group.id})][SYNC] <- ${message.content}")
+        }
+        eventChannel.subscribeAlways<FriendMessageSyncEvent> {
+            println("[好友][${sender.remarkOrNick}(${sender.id})][SYNC] <- ${message.content}")
+        }
         logger.info { "Moyu Plugin loaded" }
 
     }
